4465ac361847aa77a18abd7295242a77b7eeeafb
omangbaheti <omangbaheti@gmail.com>
2024-09-20 10:37:25 -0700

    Adding some comments

--- Assets/_Scripts/BubbleCursor.cs
+++ Assets/_Scripts/BubbleCursor.cs
@@ -27,10 +27,13 @@ public class BubbleCursor : MonoBehaviour
 
         float minDistance = Mathf.Infinity;
         Collider2D detectedCollider = null;
+
         Physics2D.OverlapCircle(transform.position, radius, contactFilter, results);
 
+        //Detect the closest target
         foreach (Collider2D col in results)
         {
+            //Change a vector3 to a Vector 2 since we're working in 2D
             Vector2 flattenedTransform = new(transform.position.x, transform.position.y);
             Vector2 flattenedColliderPosition = new(col.transform.position.x, col.transform.position.y);
             float distance = (flattenedColliderPosition - flattenedTransform).magnitude;
@@ -41,7 +44,9 @@ public class BubbleCursor : MonoBehaviour
             }
         }
 
+        //Get Mouse Position on screen, and get the corresponding position in a Vector3 World Co-Ordinate
         Vector3 mousePosition = Input.mousePosition;
+        //Change the z position so that cursor does not get occluded by the camera
         mousePosition.z += 10f;
         mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, Screen.width);
         mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
@@ -49,12 +54,15 @@ public class BubbleCursor : MonoBehaviour
 
         if (detectedCollider == null)
         {
-
             return;
         }
 
+        //Expand the bubble visual to cover the desired target
+        //Also highlights the closest target to yellow
         ExpandBubble(detectedCollider, minDistance);
 
+        //Checking if the target detected in previous and current frame are the same
+        //If target changes, change the previous target back to white
         if (previousDetectedCollider != null && detectedCollider != previousDetectedCollider)
         {
             if (previousDetectedCollider.TryGetComponent(out Target t))
@@ -63,7 +71,7 @@ public class BubbleCursor : MonoBehaviour
             }
         }
 
-
+        //On Mouse Click, select the closest target
         if (Input.GetMouseButtonDown(0))
         {
             SelectTarget(detectedCollider);
@@ -93,6 +101,8 @@ public class BubbleCursor : MonoBehaviour
         }
     }
 
+
+    //Debug code
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, radius);
